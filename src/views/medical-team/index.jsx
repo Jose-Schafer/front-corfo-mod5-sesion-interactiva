dd67878a3cbebfa3754a015cf37a8e8e0bff40e9
Jose Schafer <joseignacio.schafer@gmail.com>
2025-01-13 21:54:59 -0300

    Fix error

--- src/views/medical-team/index.jsx
+++ src/views/medical-team/index.jsx
@@ -43,7 +43,7 @@ export default function MedicalTeam() {
       />
       <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 xl:grid-cols-6 gap-4">
         {shownDoctors.map((doctor, key) => (
-          <DoctorCard key={key} doctor={doctor} />
+          <DoctorCard key={key} doctorData={doctor} />
         ))}
       </div>
     </div>
